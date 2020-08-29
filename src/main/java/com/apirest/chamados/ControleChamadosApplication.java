03cbb8045597c2a901e20c99b9c770f30055f1a4
danielsilvalima1996 <dsl15021996@gmail.com>
2020-08-29 15:23:04 -0300

    first commit

---
+++ src/main/java/com/apirest/chamados/ControleChamadosApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleChamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirest/chamados/ControleChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
