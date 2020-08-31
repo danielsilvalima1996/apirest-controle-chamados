584925eb86de2c40c62b3403268b1da1bd3bcb85
danielsilvalima1996 <dsl15021996@gmail.com>
2020-08-31 00:10:33 -0300

    empresa model inicial

---
+++ src/main/java/com/apirest/chamados/model/Empresa.java
@@ -0,0 +1,155 @@
+package com.apirest.chamados.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "cnpj", length = 14, nullable = false, unique = true)
+	@NotNull
+	private String cnpj;
+
+	@Column(name = "razaoSocial", length = 100, nullable = false)
+	@NotNull
+	private String razaoSocial;
+
+	@Column(name = "nomeFantasia", length = 100, nullable = false)
+	@NotNull
+	private String nomeFantasia;
+
+	@Column(name = "idEndereco")
+	private long idEndereco;
+
+	@CreatedDate
+	@Column(name = "criado", updatable = false)
+	private Date criado;
+
+	@LastModifiedDate
+	@Column(name = "modificado")
+	private Date modificado;
+
+	@CreatedBy
+	@Column(name = "criadoPor", updatable = false)
+	private String criadoPor;
+
+	@LastModifiedDate
+	@Column(name = "modificadoPor")
+	private String modificadoPor;
+
+	@Column(name = "ativo", nullable = false)
+	@NotNull
+	private boolean ativo;
+	
+	@Fetch(FetchMode.SELECT)
+	@OneToMany(mappedBy = "idEmpresa", fetch = FetchType.EAGER)
+	private List<Usuario> idUsuario = new ArrayList<Usuario>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public long getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(long idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public Date getCriado() {
+		return criado;
+	}
+
+	public void setCriado(Date criado) {
+		this.criado = criado;
+	}
+
+	public Date getModificado() {
+		return modificado;
+	}
+
+	public void setModificado(Date modificado) {
+		this.modificado = modificado;
+	}
+
+	public String getCriadoPor() {
+		return criadoPor;
+	}
+
+	public void setCriadoPor(String criadoPor) {
+		this.criadoPor = criadoPor;
+	}
+
+	public String getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(String modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
