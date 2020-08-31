96cb272b351e564b03e938cf67f71299f71bd2bd
danielsilvalima1996 <dsl15021996@gmail.com>
2020-08-31 00:09:51 -0300

    logica login

---
+++ src/main/java/com/apirest/chamados/controller/JwtAuthenticationController.java
@@ -0,0 +1,72 @@
+package com.apirest.chamados.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.chamados.config.JwtTokenUtil;
+import com.apirest.chamados.model.JwtRequest;
+import com.apirest.chamados.model.Usuario;
+import com.apirest.chamados.repository.UsuarioRepository;
+import com.apirest.chamados.service.JwtUserDetailsService;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/login")
+public class JwtAuthenticationController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@ApiOperation(value = "Endpoint login")
+	@PostMapping()
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+		Usuario user = repository.findByEmail(authenticationRequest.getUsername());
+		authentication(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		Map<Object, Object> model = new HashMap<>();
+		model.put("token", token);
+		model.put("email", authenticationRequest.getUsername());
+		model.put("nomeCompleto", user.getNomeCompleto());
+		model.put("avatar", user.getAvatar());
+		model.put("id", user.getId());
+		model.put("ativo", user.isAtivo());
+
+		return ResponseEntity.ok(model);
+	}
+
+	private void authentication(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+}
---
+++ src/main/java/com/apirest/chamados/service/JwtUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.apirest.chamados.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.apirest.chamados.model.Usuario;
+import com.apirest.chamados.repository.UsuarioRepository;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UsuarioRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Usuario user = repository.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("Usuário não encontrado com o email: " + email);
+		}
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority("X");
+		List<SimpleGrantedAuthority> list = new ArrayList<SimpleGrantedAuthority>();
+		list.add(authority);
+		return new User(user.getEmail(), user.getSenha(), list);
+	}
+}
