8597239381b7ef086299e972fa6f48d70d474f8d
danielsilvalima1996 <dsl15021996@gmail.com>
2020-08-31 00:09:02 -0300

    teste bcrypt

--- src/main/java/com/apirest/chamados/ControleChamadosApplication.java
+++ src/main/java/com/apirest/chamados/ControleChamadosApplication.java
@@ -2,6 +2,7 @@ package com.apirest.chamados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ControleChamadosApplication {
