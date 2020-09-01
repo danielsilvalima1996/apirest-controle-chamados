30a5afdc51493c8a9546cff89b449717119d134b
danielsilvalima1996 <dsl15021996@gmail.com>
2020-09-01 20:02:31 -0300

    alterado nome coluna

---
+++ src/main/resources/db/migration/V1.020__ALTER_TABLE_EMPRESA_ALTER_CAMPOS.sql
@@ -0,0 +1,5 @@
+ALTER TABLE empresa 	
+CHANGE razaoSocial razao_social VARCHAR(100),
+CHANGE nomeFantasia nome_fantasia VARCHAR(100),
+CHANGE modificadoPor modificado_por VARCHAR(255),
+CHANGE criadoPor criado_por VARCHAR(255);
