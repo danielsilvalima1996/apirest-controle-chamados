1729538b5dd28fd258bab32ca2ac406b05f72e3b
danielsilvalima1996 <dsl15021996@gmail.com>
2020-09-01 14:29:56 -0300

    passsando a mensagem

--- src/main/java/com/apirest/chamados/handler/CustomRestExceptionHandler.java
+++ src/main/java/com/apirest/chamados/handler/CustomRestExceptionHandler.java
@@ -106,7 +106,7 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({ Exception.class })
 	public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
-		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "error occurred");
+		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), "error occurred");
 		return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
