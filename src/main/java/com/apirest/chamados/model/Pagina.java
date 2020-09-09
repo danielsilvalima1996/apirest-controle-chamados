d755f115fed478e1221e13d592aa0f59eb2d203c
danielsilvalima1996 <dsl15021996@gmail.com>
2020-09-09 20:32:38 -0300

    add pagina

--- src/main/java/com/apirest/chamados/model/Pagina.java
+++ src/main/java/com/apirest/chamados/model/Pagina.java
@@ -1,24 +1,17 @@
 package com.apirest.chamados.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
---
+++ src/main/java/com/apirest/chamados/repository/PaginaRepository.java
@@ -0,0 +1,9 @@
+package com.apirest.chamados.repository;
+
+import com.apirest.chamados.model.Pagina;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaginaRepository extends JpaRepository<Pagina,Long> {
+    
+}
