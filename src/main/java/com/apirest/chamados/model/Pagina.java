8c55432b922714363a32937d4c422c6e994b188a
danielsilvalima1996 <dsl15021996@gmail.com>
2020-09-09 20:06:51 -0300

    add model pagina

---
+++ src/main/java/com/apirest/chamados/model/Pagina.java
@@ -0,0 +1,168 @@
+package com.apirest.chamados.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "pagina")
+public class Pagina implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@NotNull
+	@Column(name = "link", length = 50, nullable = false)
+	private String link;
+
+	@NotNull
+	@Column(name = "icon", length = 50, nullable = false)
+	private String icon;
+
+	@NotNull
+	@Column(name = "label", length = 50, nullable = false)
+	private String label;
+
+	@NotNull
+	@Column(name = "short_label", length = 50, nullable = false)
+	private String shortLabel;
+
+	@NotNull
+	@Column(name = "ativo", nullable = false)
+	private boolean ativo;
+
+	@CreatedDate
+	@Column(name = "criado", updatable = false)
+	private Date criado;
+
+	@LastModifiedDate
+	@Column(name = "modificado")
+	private Date modificado;
+
+	@CreatedBy
+	@Column(name = "criado_por", updatable = false)
+	private String criadoPor;
+
+	@LastModifiedBy
+	@Column(name = "modificado_por")
+	private String modificadoPor;
+
+	@NotNull
+	@Column(name = "parent", length = 50, nullable = false)
+	private String parent;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getShortLabel() {
+		return shortLabel;
+	}
+
+	public void setShortLabel(String shortLabel) {
+		this.shortLabel = shortLabel;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Date getCriado() {
+		return criado;
+	}
+
+	public void setCriado(Date criado) {
+		this.criado = criado;
+	}
+
+	public Date getModificado() {
+		return modificado;
+	}
+
+	public void setModificado(Date modificado) {
+		this.modificado = modificado;
+	}
+
+	public String getCriadoPor() {
+		return criadoPor;
+	}
+
+	public void setCriadoPor(String criadoPor) {
+		this.criadoPor = criadoPor;
+	}
+
+	public String getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(String modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+
+	public String getParent() {
+		return parent;
+	}
+
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+
+}
