cc2857acec0bbe87554a073dd34d539986fb9fcc
danielsilvalima1996 <dsl15021996@gmail.com>
2020-09-09 18:58:01 -0300

    adicionado flyway insert tipo e subtipo

---
+++ src/main/resources/db/migration/V1.015__INSERT_TABLE_SUBTIPO_CHAMADO.sql
@@ -0,0 +1,4 @@
+INSERT INTO subtipo_chamado 
+	(ativo, criado, modificado, criado_por, modificado_por, descricao, id_tipo_chamado )
+VALUES
+	(1, now(), now(), 'FLYWAY SYSTEM', 'FLYWAY', 'TESTE', 1);
