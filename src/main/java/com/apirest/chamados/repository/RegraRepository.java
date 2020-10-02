6371f23b4132c33af1174fd28926a7799be4157d
danielsilvalima1996 <dsl15021996@gmail.com>
2020-10-01 21:11:35 -0300

    removido imports errados

--- src/main/java/com/apirest/chamados/repository/RegraRepository.java
+++ src/main/java/com/apirest/chamados/repository/RegraRepository.java
@@ -6,8 +6,6 @@ import com.apirest.chamados.model.Regra;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RegraRepository extends JpaRepository<Regra, Long> {
 	
