9074a06f7b751228d2bd6e0361c55244dfdb62bb
dsl15021996@gmail.com <daniel.lima@ogrupogc.com>
2020-10-16 13:20:35 -0300

    resolvido error menu no value

--- src/main/java/com/apirest/chamados/service/MenuPOUIService.java
+++ src/main/java/com/apirest/chamados/service/MenuPOUIService.java
@@ -18,17 +18,20 @@ public class MenuPOUIService {
 
 	public List<MenuPOUI> criaMenu(Long idRegra) throws Exception {
 		List<Pagina> paginas = this.regraService.findById(idRegra).get().getIdPagina();
-		paginas.stream().sorted((a, b) -> a.getParent().compareTo(b.getParent()));
+		// paginas.stream().sorted((a, b) -> a.getParent().compareTo(b.getParent()));
 		List<MenuPOUI> menu = new ArrayList<>();
-		for (Pagina p : paginas) {
-			if (p.getParent() == 0) {
+		if (paginas.size() > 0) {
+			List<Pagina> parent = paginas.stream().filter(item -> item.getParent() == 0).collect(Collectors.toList());
+			List<Pagina> children = paginas.stream().filter(item -> item.getParent() != 0).collect(Collectors.toList());
+			for (Pagina p : parent) {
 				menu.add(new MenuPOUI(p.getId(), p.getIcon(), p.getLabel(), p.getShortLabel(), p.getLink(),
 						new ArrayList<>()));
-			} else {
+			}
+			for (Pagina p : children) {
 				MenuPOUI pai = menu.stream().filter(item -> item.getId() == p.getParent()).findFirst().get();
 				List<MenuPOUI> aux = pai.getSubItems();
-				aux.add(new MenuPOUI(p.getId(), p.getIcon(), p.getLabel(), p.getShortLabel(),
-						p.getLink(), new ArrayList<>()));
+				aux.add(new MenuPOUI(p.getId(), p.getIcon(), p.getLabel(), p.getShortLabel(), p.getLink(),
+						new ArrayList<>()));
 				aux = aux.stream().sorted((a, b) -> a.getLabel().compareTo(b.getLabel())).collect(Collectors.toList());
 				pai.setSubItems(aux);
 			}
