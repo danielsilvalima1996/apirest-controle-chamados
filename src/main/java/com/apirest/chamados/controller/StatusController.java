42791ab9c233e31127dae7ca30a39d8d8a44e779
danielsilvalima1996 <dsl15021996@gmail.com>
2022-02-25 21:25:13 -0300

    - Ajustado status - Alterado application

--- src/main/java/com/apirest/chamados/controller/StatusController.java
+++ src/main/java/com/apirest/chamados/controller/StatusController.java
@@ -4,11 +4,13 @@ import java.util.Date;
 
 import com.apirest.chamados.model.dto.Status;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -19,14 +21,16 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/api/status")
 public class StatusController {
 
-    // @Autowired
-	// private RegraService service;
+	@Value("${spring.datasource.url}")
+	private String database;
 
 	@ApiOperation(value = "Status da API")
 	@GetMapping(produces = { "application/json" })
-	public ResponseEntity<Status> findAll() throws Exception {
-		Status status = new Status("url", "PostgreSQL", new Date());
-        return ResponseEntity.ok(status);
+	public ResponseEntity<Status> getStatus() throws Exception {
+		final String baseUrl = 
+		ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+		String[] baseName = database.split(":");
+		return ResponseEntity.ok(new Status(baseUrl, baseName[1].toUpperCase(), new Date()));
 	}
     
 }
