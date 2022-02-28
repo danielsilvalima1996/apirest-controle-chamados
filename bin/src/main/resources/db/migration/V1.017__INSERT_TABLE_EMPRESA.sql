b530105bfbad8846b26f9c32fb11ab461e33bde7
danielsilvalima1996 <dsl15021996@gmail.com>
2022-02-28 18:18:53 -0300

    - removido flyway migration - removido bin - ajustado chamados

--- bin/src/main/resources/db/migration/V1.017__INSERT_TABLE_EMPRESA.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO empresa 
-	(id, cnpj , razaoSocial , nomeFantasia, criado , modificado, criadoPor, modificadoPor, ativo)
-VALUES
-	(1, '12345678901234', 'Empresa Teste S.A', 'Fantasia Teste', now(), now(), '', '', 1);
--- bin/src/main/resources/db/migration/V1.07__ALTER_TABLE_EMPRESA_NOT_UNIQUE_RAZAO_SOCIAL.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE empresa 
-drop index razaoSocial;
