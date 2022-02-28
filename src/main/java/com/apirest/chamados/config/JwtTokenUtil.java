71431b7fdbcb48d2a5ffbb1c1e6a0e32bc3a21f3
danielsilvalima1996 <dsl15021996@gmail.com>
2022-02-28 20:43:58 -0300

    - removido log desnecessário - ajustado serviço

--- src/main/java/com/apirest/chamados/config/JwtTokenUtil.java
+++ src/main/java/com/apirest/chamados/config/JwtTokenUtil.java
@@ -58,7 +58,6 @@ public class JwtTokenUtil implements Serializable {
 
 	// Cria o token e devine tempo de expiração pra ele
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
-		System.out.println(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000));
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
 				.signWith(SignatureAlgorithm.HS512, secret).compact();
